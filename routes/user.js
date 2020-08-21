44e81456d73d4b18ec7b6462ebf4d3db940cb167
Noiret <sergio.ko@icloud.com>
2020-08-21 10:42:03 +0200

    Update

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+// HELPER FUNCTIONS
+const {
+  isLoggedIn,
+  isNotLoggedIn,
+  validationLoggin,
+} = require("../helpers/middlewares");
+
+
+// GET '/profile'
+router.get("/profile", isLoggedIn(), (req, res, next) => {
+  res.status(200).json({ message: "You are on your profile!" });
+});
 
 
 module.exports = router;
