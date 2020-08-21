2fbfd6c3e36deb815f9af4f191f97802f91bff64
Jean-Baptiste Castillo <castillo.jbaptiste@gmail.com>
2020-08-21 15:48:02 +0200

    marketplace route done

--- routes/marketplace.js
+++ routes/marketplace.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+// HELPER FUNCTIONS
+const {
+  isLoggedIn,
+  isNotLoggedIn,
+  validationLoggin,
+} = require("../helpers/middlewares");
+
 // GET '/marketplace'
 router.get("/marketplace", isLoggedIn(), (req, res, next) => {
   res.status(200).json({ message: "You are on the marketplace page!" });
