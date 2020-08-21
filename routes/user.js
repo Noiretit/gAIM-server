b7c4784903abd217658e9bd2f8f027ab002ac81f
Jean-Baptiste Castillo <castillo.jbaptiste@gmail.com>
2020-08-21 10:35:58 +0200

    D:/Git/videogames route done

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+// HELPER FUNCTIONS
+const {
+  isLoggedIn,
+  isNotLoggedIn,
+  validationLoggin,
+} = require("../helpers/middlewares");
 
+// GET '/myprofile'
+router.get("/myprofile", isLoggedIn(), (req, res, next) => {
+  res.status(200).json({ message: "You are on your profile!" });
+});
+
+// GET '/videogames'
+router.get("/videogames", isLoggedIn(), (req, res, next) => {
+  res.status(200).json({ message: "You are on the videogames page!" });
+});
 
 module.exports = router;
