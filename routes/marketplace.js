a882086b8badd6c8c7f4882190eb178b135fd672
Jean-Baptiste Castillo <castillo.jbaptiste@gmail.com>
2020-08-21 10:59:09 +0200

    D:/Git/marketplace route done

--- routes/marketplace.js
+++ routes/marketplace.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+// GET '/marketplace'
+router.get("/marketplace", isLoggedIn(), (req, res, next) => {
+  res.status(200).json({ message: "You are on the marketplace page!" });
+});
+
 module.exports = router;
